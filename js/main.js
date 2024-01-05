4067543bad322e22c372730e003e324db878ea2e
nourelnaby4 <nourelnaby4@gmail.com>
2024-01-06 01:14:50 +0200

    add Resume Section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-showMenue();
+$(document).ready(function(){
+    showMenue();
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        // autoplayHoverPause:true
+    });
+});
 
 function showMenue(){
     const menuToggle = document.getElementById('toggleMobileMenu');
-const menu = document.getElementById('menu');
-menuToggle.addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menuToggle.addEventListener('click', function() {
     menu.classList.toggle('show-mobile');
-});
+    });
 }
